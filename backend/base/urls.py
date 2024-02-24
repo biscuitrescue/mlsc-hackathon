1a79a1dc1605320dba3848de8aa09c2de6768261
biscuitrescue <karttikeyasinha11@gmail.com>
2024-02-25 04:59:17 +0530

    added api_view

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
     path('students/', views.student_list, name='students'),
     path('stand/', views.stand_list, name='stands'),
+    path('students/<int:id>', views.student_deets, name='students_deets'),
+    path('stand/<int:id>', views.stand_deets, name='stands_deets'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Student, Stand
 from .serializer import StudentSerializer, StandSerializer
-from rest_framework.decorators import api_view
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 # from django.http import HttpResponse
@@ -34,7 +35,8 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
-# @api_view(['GET', 'POST'])
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.AllowAny])
 def student_list(request):
 
     if request.method == 'GET':
@@ -49,6 +51,8 @@ def student_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.AllowAny])
 def stand_list(request):
 
     if request.method == 'GET':
@@ -63,5 +67,43 @@ def stand_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# def student_deets():
-#     pass
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([permissions.AllowAny])
+def student_deets(request, id):
+
+    try:
+        student = Student.objects.get(pk=id)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+    # elif request.method == 'PUT':
+    #     pass
+    # elif request.method == 'DELETE':
+    #     pass
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([permissions.AllowAny])
+def stand_deets(request, id):
+
+    try:
+        stand = Stand.objects.get(pk=id)
+    except Stand.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StandSerializer(stand)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = StandSerializer(stand, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        stand.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
