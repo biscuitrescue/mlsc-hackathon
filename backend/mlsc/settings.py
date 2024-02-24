1d37fb8abf498d052318c9cebd4226fcb2697953
biscuitrescue <karttikeyasinha11@gmail.com>
2024-02-25 00:15:34 +0530

    added rest

--- backend/mlsc/settings.py
+++ backend/mlsc/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'base.apps.BaseConfig'
 ]
 
@@ -103,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
