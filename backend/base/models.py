ae2065e5a917f943a3647060930e3de1d8b5d553
biscuitrescue <karttikeyasinha11@gmail.com>
2024-02-25 00:52:09 +0530

    models

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=30)
     roll = models.CharField(max_length=9)
+    location = models.CharField(max_length=50)
+
+
+class Stand(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=50)
