04019145f03030fbc1f1a47ee40c2860ce585438
Amritesh Bhardwaj <bhamritesh@gmail.com>
2024-02-24 22:26:24 +0530

    new

--- base-app/App.js
+++ base-app/App.js
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5b59a6',
     alignItems: 'center',
     justifyContent: 'center',
+    resizeMode: ''
   },
 });
