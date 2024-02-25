d2934200dbdbc2629880ddddc72140053a5da567
biscuitrescue <karttikeyasinha11@gmail.com>
2024-02-25 06:01:29 +0530

    db

--- backend/base/db.py
+++ backend/base/db.py
@@ -5,8 +5,12 @@ obj = con.connect(
     user='root',
     password='Pantera@101',
     host='localhost',
-    database='PROJECT'
+    database="PROJECT"
 )
 
-
 cur1 = obj.cursor()
+
+q1 = "select * from student"
+
+cur1.execute(q1)
+print(cur1.fetchall())
